3c9ba36702319288baf6b8dbddadef2db9a9146a
Khaled <jumahkhaledhassan@gmail.com>
2026-07-29 16:54:03 +0300

    Add files via upload

---
+++ new 1-4.js
@@ -0,0 +1,28 @@
+<html>
+<head>
+    
+    <title>Contact Me</title>
+    <link rel="stylesheet" href="new 1-3.css">
+</head>
+<body>
+
+    <form id="contactForm" onsubmit="return validateForm()">
+        <label for="fullname">Full Name:</label><br>
+        <input type="text" id="fullname" name="fullname"><br><br>
+
+        <label for="email">Email Address:</label><br>
+        <input type="email" id="email" name="email"><br><br>
+
+        <label for="country">Country:</label><br>
+        <input type="text" id="country" name="country"><br><br>
+
+        <label for="comments">Comments:</label><br>
+        <textarea id="comments" name="comments" rows="4"></textarea><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+	
+	<script src="new 1-4.js"></script>
+
+</body>
+</html>
